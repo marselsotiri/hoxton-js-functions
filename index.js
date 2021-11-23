c82a0ccc18ecb4f7a4e7c4dc9d7be186d438535e
Marsel <you@example.com>
2021-11-23 16:12:06 +0100

    commit

--- index.js
+++ index.js
@@ -63,3 +63,57 @@ function admit(age=Number(),name='') {
   console.log(isEven(myNumber));
 
   console.log(admit(16,'Beni'));
+
+
+  /* WRITE THE CODE FOR THE FOLLOWING FUNCTIONS */
+
+// Input: a (number), b (number)
+// Action: Add both numbers together
+// Output: The result (number)
+function add(a, b) {
+  return a + b
+}
+
+// Input: number
+// Action: Check if the number given is even or not
+// Output: The result (boolean)
+function isEven(number) {
+  return number % 2 === 0
+}
+
+// Input: name (string)
+// Action: Greet the user by name
+// Output: The greeting (string)
+function greet(name) {
+  return `Hi, ${name}!`
+}
+
+// Input: age (number)
+// Action: Check if the age is 18 or over
+// Output: The result (boolean)
+function isAnAdult(age) {
+  return age >= 18
+}
+
+// Input: age (number)
+// Action: Check how many years are left until adulthood (18)
+// Output: The result (number)
+function yearsToAdulthood(age) {
+  return 18 - age
+}
+
+// Input: person ({ age: number, name: string })
+// Action:
+//   - check if a person is an adult
+//   - if they are, greet them
+//   - if they are not, tell them to come back in X years (when they are)
+// Output: The result (string)
+function admit(person) {
+  if (isAnAdult(person.age)) {
+    const greeting = greet(person.name)
+    console.log(greeting)
+  } else {
+    const years = yearsToAdulthood(person.age)
+    console.log(`Sorry, come back in ${years} years.`)
+  }
+}
